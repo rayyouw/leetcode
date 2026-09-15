bd8decb52a1733d4ea28a9c57177ac2270378ced
Rayhan Fairuz Aqram <rayhan.fairuz51@ui.ac.id>
2026-09-15 20:52:57 +0700

    Time: 427 ms (5.03%) | Memory: 48.1 MB (8.63%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,14 +1,16 @@
-import java.util.*;
 class Solution {
     public boolean canJump(int[] nums) {
-        int maxin = 0;
-        for(int i = 0; i < nums.length; i++){
-            if(maxin < i){
-                return false;
+        int n = nums.length;
+        boolean[] dp = new boolean[n];
+        dp[0] = true;
+        
+        for (int i = 0; i < n; i++) {
+            if (!dp[i]) continue;
+            for (int step = 1; step <= nums[i] && i + step < n; step++) {
+                dp[i + step] = true;
             }
-            maxin = Math.max(maxin, i + nums[i]);
         }
-
-        return true;
+        
+        return dp[n - 1];
     }
 }
