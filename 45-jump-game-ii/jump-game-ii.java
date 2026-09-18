0808b345fccadafbfe4731f16536bde36714caf8
Rayhan Fairuz Aqram <rayhan.fairuz51@ui.ac.id>
2026-09-18 10:28:57 +0700

    Time: 183 ms (5.17%) | Memory: 46.8 MB (96.57%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,23 +1,25 @@
 import java.util.*;
+
 class Solution {
     public int jump(int[] nums) {
         int n = nums.length;
-        int[] dp = new int[n + 5];
+        int[] dp = new int[10005];
+        int[] c = new int[10005];
+        c[n-1] = 1;
+        Arrays.fill(dp, 100000);
+        dp[n-1] = 0;
 
-        for(int i = 0; i <= n; i++) dp[i] = Integer.MAX_VALUE;
-        dp[0] = 0;
-        
-        for(int i = 0; i < n; i++){
+        for(int i = n-2; i >= 0; i--){
             for(int j = 1; j <= nums[i]; j++){
-                if(i + j < n){
-                    dp[i+j] = Math.min(dp[i+j], dp[i] + 1);
-                } 
+                if(i + j < n) dp[i] = Math.min(dp[i], 1 + dp[i+j]);
             }
+
+            System.out.println(dp[i]);
         }
 
-        for(int i = 0; i < n; i++){
-            System.out.println("Nilai ke-" + i + " : " + dp[i]);
-        }
-        return dp[n-1];
+        return dp[0];
     }
 }
+
+// Lakukan iterasi dari index (posisi) terakhir (n-1)
+// untuk setiap iterasinya (selain iterasi index terakhir), cek semua index yang dia bisa jump kesitu. Misalkan :
