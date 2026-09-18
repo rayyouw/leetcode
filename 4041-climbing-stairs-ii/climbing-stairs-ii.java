db250d9e547cacfff2b5cc1547a843005fb3750a
Rayhan Fairuz Aqram <rayhan.fairuz51@ui.ac.id>
2026-09-18 11:04:15 +0700

    Time: 7 ms (69.86%) | Memory: 116.6 MB (82.57%) - LeetSync

--- 4041-climbing-stairs-ii/climbing-stairs-ii.java
+++ 4041-climbing-stairs-ii/climbing-stairs-ii.java
@@ -1,22 +1,18 @@
+import java.util.*;
+
 class Solution {
     public int climbStairs(int n, int[] costs) {
-        int[] dp = new int[n + 5];
+        int[] dp = new int[n+5];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = 0;
-        for(int i = 1; i <= n; i++) dp[i] = Integer.MAX_VALUE/2;
-        for(int i = 1; i <= n; i++){
-            if(i-1 >= 0){
-                dp[i] = Math.min(dp[i], dp[i-1] + costs[i-1] + 1 );
-            }
-
-            if(i-2 >= 0){
-                dp[i] = Math.min(dp[i], dp[i-2] + costs[i-1] + 4);
-            }
-
-            if(i-3 >= 0){
-                dp[i] = Math.min(dp[i], dp[i-3] + costs[i-1] + 9);
-            }
-
-            // System.out.println("Hasil ke-" + i + " : " + dp[i]);
+        dp[1] = costs[0] + 1;
+        // System.out.println(0 + " : " + dp[0]);
+        // System.out.println(1 + " : " + dp[1]);
+        for(int i = 2; i <= n; i++){
+            if(i-1 >= 0) dp[i] = Math.min(dp[i], dp[i-1] + costs[i-1] + 1);
+            if(i-2 >= 0) dp[i] = Math.min(dp[i], dp[i-2] + costs[i-1] + 4);
+            if(i-3 >= 0) dp[i] = Math.min(dp[i], dp[i-3] + costs[i-1] + 9);
+            // System.out.println(i + " : " + dp[i]);
         }
 
         return dp[n];
