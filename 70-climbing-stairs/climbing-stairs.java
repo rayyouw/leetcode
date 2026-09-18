e5b0b48dc5aa34a33b7f3359f9ee6a07688f77af
Rayhan Fairuz Aqram <rayhan.fairuz51@ui.ac.id>
2026-09-18 10:49:39 +0700

    Time: 0 ms (100.00%) | Memory: 42.1 MB (54.71%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,13 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[46];
+        int[] dp = new int[n+5];
 
-        dp[0] = 1;
         dp[1] = 1;
         dp[2] = 2;
-        for(int i = 3; i <= n; i++){
+
+        for(int i = 3; i <= n+1; i++){
             dp[i] = dp[i-1] + dp[i-2];
         }
 
